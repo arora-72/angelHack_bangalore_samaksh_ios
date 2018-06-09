94cd58918d06aa262252ebccc8033eb61cf9d814
arora-72 <indresh.arora2016@vitstudent.ac.in>
2018-06-09 16:08:13 +0530

    Initial Commit

---
+++ Rinnegan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rinnegan
+//
+//  Created by Indresh Arora on 09/06/18.
+//  Copyright © 2018 Indresh Arora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Rinnegan/previewCameraViewController.swift
@@ -0,0 +1,81 @@
+//
+//  previewCameraViewController.swift
+//  Rinnegan
+//
+//  Created by Indresh Arora on 09/06/18.
+//  Copyright © 2018 Indresh Arora. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import Photos
+
+class previewCameraViewController: UIViewController {
+    
+    let cameraController = CameraController()
+    
+    var captureSession: AVCaptureSession?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+
+    @IBOutlet weak var previewView: UIView!
+    @IBOutlet fileprivate var photoModeButton: UIButton!
+    @IBOutlet fileprivate var toggleCameraButton: UIButton!
+    @IBOutlet fileprivate var toggleFlashButton: UIButton!
+    
+    ///Allows the user to put the camera in video mode.
+    @IBOutlet fileprivate var videoModeButton: UIButton!
+    
+    @IBOutlet weak var captureButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+        func styleCaptureButton() {
+            captureButton.layer.borderColor = UIColor.black.cgColor
+            captureButton.layer.borderWidth = 2
+            
+            captureButton.layer.cornerRadius = min(captureButton.frame.width, captureButton.frame.height) / 2
+        }
+        
+        func configureCameraController() {
+            cameraController.prepare {(error) in
+                if let error = error {
+                    print(error)
+                }
+                
+                try? self.cameraController.displayPreview(on: self.previewView)
+            }
+        }
+        styleCaptureButton()
+        configureCameraController()
+        
+    }
+    
+    
+    
+    @IBAction func captureImage(_ sender: UIButton) {
+        cameraController.captureImage {(image, error) in
+            guard let image = image else {
+                print(error ?? "Image capture error")
+                return
+            }
+            
+            try? PHPhotoLibrary.shared().performChangesAndWait {
+                PHAssetChangeRequest.creationRequestForAsset(from: image)
+            }
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
